9eff8e41db3e46c24c2324cd89c7f57d1010089a
drewjenks01 <awj@mit.edu>
2023-10-03 15:17:00 -0400

    update

--- navigation/demo/utils.py
+++ navigation/demo/utils.py
@@ -7,4 +7,13 @@ def make_partial_run_label(partial_run_count: int) -> str:
     return constants.DEMO_PARTIAL_RUN_LABEL+str(partial_run_count)+'.pkl'
 
 def get_empty_demo_data() -> None:
-    return {constants.DEMO_RGB_KEY: [], constants.DEMO_DEPTH_KEY: [], constants.DEMO_COMMAND_KEY: []}
+    demo_data = {}
+
+    # add command key
+    demo_data[constants.COMMAND_KEY] = []
+
+    # add image keys
+    for cam_name in constants.CAMERA_IMAGE_NAMES:
+        demo_data[cam_name] = []
+        
+    return demo_data

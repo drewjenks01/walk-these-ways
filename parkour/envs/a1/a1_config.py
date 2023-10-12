52716a989a8c7e0e46267cea43b76a65029ab1d4
drewjenks01 <awj@mit.edu>
2023-10-12 17:25:40 -0400

    update

--- parkour/envs/a1/a1_config.py
+++ parkour/envs/a1/a1_config.py
@@ -28,7 +28,7 @@
 #
 # Copyright (c) 2021 ETH Zurich, Nikita Rudin
 
-from parkour.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
+from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
 
 class A1RoughCfg( LeggedRobotCfg ):
     class init_state( LeggedRobotCfg.init_state ):
@@ -50,37 +50,18 @@ class A1RoughCfg( LeggedRobotCfg ):
             'RR_calf_joint': -1.5,    # [rad]
         }
 
-    class init_state_slope( LeggedRobotCfg.init_state ):
-        pos = [0.5, 0.0, 0.24] # x,y,z [m]
-        default_joint_angles = { # = target angles [rad] when action = 0.0
-            'FL_hip_joint': 0.03,   # [rad]
-            'RL_hip_joint': 0.03,   # [rad]
-            'FR_hip_joint': -0.03,  # [rad]
-            'RR_hip_joint': -0.03,   # [rad]
-
-            'FL_thigh_joint': 1.0,     # [rad]
-            'RL_thigh_joint': 1.8,   # [rad]1.8
-            'FR_thigh_joint': 1.0,     # [rad]
-            'RR_thigh_joint': 1.8,   # [rad]
-
-            'FL_calf_joint': -2.2,   # [rad]
-            'RL_calf_joint': -1.2,    # [rad]
-            'FR_calf_joint': -2.2,  # [rad]
-            'RR_calf_joint': -1.2,    # [rad]
-        }
-        
     class control( LeggedRobotCfg.control ):
         # PD Drive parameters:
         control_type = 'P'
-        stiffness = {'joint': 20.}  # [N*m/rad]
+        stiffness = {'joint': 30.}  # [N*m/rad]
         damping = {'joint': 0.5}     # [N*m*s/rad]
         # action scale: target angle = actionScale * action + defaultAngle
-        action_scale = 0.25
+        action_scale = 0.5
         # decimation: Number of control action updates @ sim DT per policy DT
         decimation = 4
 
     class asset( LeggedRobotCfg.asset ):
-        file = './resources/robots/go1/urdf/go1.urdf'
+        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/a1/urdf/a1.urdf'
         foot_name = "foot"
         penalize_contacts_on = ["thigh", "calf"]
         terminate_after_contacts_on = ["base"]#, "thigh", "calf"]
@@ -89,9 +70,9 @@ class A1RoughCfg( LeggedRobotCfg ):
     class rewards( LeggedRobotCfg.rewards ):
         soft_dof_pos_limit = 0.9
         base_height_target = 0.25
-        # class scales( LeggedRobotCfg.rewards.scales ):
-            # torques = -0.0002
-            # dof_pos_limits = -10.0
+        class scales( LeggedRobotCfg.rewards.scales ):
+            torques = 0.0
+            dof_pos_limits = -10.0
 
 class A1RoughCfgPPO( LeggedRobotCfgPPO ):
     class algorithm( LeggedRobotCfgPPO.algorithm ):
--- parkour/envs/base/legged_robot_config.py
+++ parkour/envs/base/legged_robot_config.py
@@ -31,22 +31,20 @@
 from posixpath import relpath
 from torch.nn.modules.activation import ReLU
 from torch.nn.modules.pooling import MaxPool2d
-from parkour.envs.base.base_config import BaseConfig
+from .base_config import BaseConfig
 import torch.nn as nn
 class LeggedRobotCfg(BaseConfig):
     class play:
         load_student_config = False
         mask_priv_obs = False
     class env:
-        num_envs = 6144
-
-        n_scan = 132
-        n_priv = 3+3 +3
-        n_priv_latent = 4 + 1 + 12 +12
-        n_proprio = 3 + 2 + 3 + 4 + 36 + 5
-        history_len = 10
-
-        num_observations = n_proprio + n_scan + history_len*n_proprio + n_priv_latent + n_priv #n_scan + n_proprio + n_priv #187 + 47 + 5 + 12 
+        num_envs = 4096
+        # num_observations = 48 + 45
+        # num_observations = 235 + 10 * 180 * 320
+        # num_observations = 42 + 200
+        num_observations = 235 
+        # num_observations = 235
+        # num_observations = 48 + 32 * 32
         num_privileged_obs = None # if not None a priviledge_obs_buf will be returned by step() (critic obs for assymetric training). None is returned otherwise 
         num_actions = 12
         env_spacing = 3.  # not used with heightfields/trimeshes 
@@ -54,180 +52,78 @@ class LeggedRobotCfg(BaseConfig):
         episode_length_s = 20 # episode length in seconds
         obs_type = "og"
 
-
-        
-        
-        
-        history_encoding = True
-        reorder_dofs = True
-        
-        
-        # action_delay_range = [0, 5]
-
-        # additional visual inputs 
-
-        # action_delay_range = [0, 5]
-
-        # additional visual inputs 
-        include_foot_contacts = True
-        
-        randomize_start_pos = False
-        randomize_start_vel = False
-        randomize_start_yaw = False
-        rand_yaw_range = 1.2
-        randomize_start_y = False
-        rand_y_range = 0.5
-        randomize_start_pitch = False
-        rand_pitch_range = 1.6
-
-        contact_buf_len = 100
-
-        next_goal_threshold = 0.2
-        reach_goal_delay = 0.1
-        num_future_goal_obs = 2
-
-    class depth:
         use_camera = False
-        camera_num_envs = 192
-        camera_terrain_num_rows = 10
-        camera_terrain_num_cols = 20
-
-        position = [0.27, 0, 0.03]  # front camera
-        angle = [-5, 5]  # positive pitch down
-
-        update_interval = 5  # 5 works without retraining, 8 worse
-
-        original = (106, 60)
-        resized = (87, 58)
-        horizontal_fov = 87
-        buffer_len = 2
-        
-        near_clip = 0
-        far_clip = 2
-        dis_noise = 0.0
-        
+        concatenate_depth = False
+    class depth: 
+        height = 32
+        width = 32
+        dt = 0.1
+        viz = False
+        num_depth_frames = 10
+        clip = 1
         scale = 1
-        invert = True
-
-    class normalization:
-        class obs_scales:
-            lin_vel = 2.0
-            ang_vel = 0.25
-            dof_pos = 1.0
-            dof_vel = 0.05
-            height_measurements = 5.0
-        clip_observations = 100.
-        clip_actions = 1.2
-    class noise:
-        add_noise = False
-        noise_level = 1.0 # scales other values
-        quantize_height = True
-        class noise_scales:
-            rotation = 0.0
-            dof_pos = 0.01
-            dof_vel = 0.05
-            lin_vel = 0.05
-            ang_vel = 0.05
-            gravity = 0.02
-            height_measurements = 0.02
 
     class terrain:
         mesh_type = 'trimesh' # "heightfield" # none, plane, heightfield or trimesh
-        hf2mesh_method = "grid"  # grid or fast
-        max_error = 0.1 # for fast
-        max_error_camera = 2
-
-        y_range = [-0.4, 0.4]
-        
-        edge_width_thresh = 0.05
-        horizontal_scale = 0.05 # [m] influence computation time by a lot
-        horizontal_scale_camera = 0.1
+        horizontal_scale = 0.05 # [m]
         vertical_scale = 0.005 # [m]
-        border_size = 5 # [m]
-        height = [0.02, 0.06]
-        simplify_grid = False
-        gap_size = [0.02, 0.1]
-        stepping_stone_distance = [0.02, 0.08]
-        downsampled_scale = 0.075
-        curriculum = True
-
-        all_vertical = False
-        no_flat = True
-        
+        border_size = 25 # [m]
+        height = [0.03, 0.05]
+        # height = [0.00, 0.00]
+        gap_size = [0.15, 0.20]
+        stepping_stone_distance = [0.05, 0.1]
+        downsampled_scale = 0.05
+        curriculum = False
         static_friction = 1.0
         dynamic_friction = 1.0
         restitution = 0.
+        # rough terrain only:
         measure_heights = True
-        measured_points_x = [-0.45, -0.3, -0.15, 0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.05, 1.2] # 1mx1.6m rectangle (without center line)
-        measured_points_y = [-0.75, -0.6, -0.45, -0.3, -0.15, 0., 0.15, 0.3, 0.45, 0.6, 0.75]
-        measure_horizontal_noise = 0.0
-
+        measured_points_x = [-0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8] # 1mx1.6m rectangle (without center line)
+        measured_points_y = [-0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5]
+        # measured_points_x = [-0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8]
+        # measured_points_y = [-0.4, -0.2, 0., 0.2, 0.4]
         selected = False # select a unique terrain type and pass all arguments
         terrain_kwargs = None # Dict of arguments for selected terrain
         max_init_terrain_level = 5 # starting curriculum state
-        terrain_length = 18.
-        terrain_width = 4
-        num_rows= 10 # number of terrain rows (levels)  # spreaded is benifitiall !
-        num_cols = 40 # number of terrain cols (types)
-        
-        terrain_dict = {"smooth slope": 0., 
-                        "rough slope up": 0.0,
-                        "rough slope down": 0.0,
-                        "rough stairs up": 0., 
-                        "rough stairs down": 0., 
-                        "discrete": 0., 
-                        "stepping stones": 0.0,
-                        "gaps": 0., 
-                        "smooth flat": 0,
-                        "pit": 0.0,
-                        "wall": 0.0,
-                        "platform": 0.,
-                        "large stairs up": 0.,
-                        "large stairs down": 0.,
-                        "parkour": 0.2,
-                        "parkour_hurdle": 0.2,
-                        "parkour_flat": 0.2,
-                        "parkour_step": 0.2,
-                        "parkour_gap": 0.2,
-                        "demo": 0.0,}
-        terrain_proportions = list(terrain_dict.values())
-        
+        terrain_length = 8.
+        terrain_width = 8.
+        num_rows= 10 # number of terrain rows (levels)
+        num_cols = 20 # number of terrain cols (types)
+        # terrain types: [smooth slope, 
+        #                 rough slope, 
+        #                 rough stairs up, 
+        #                 rough stairs down, 
+        #                 discrete, 
+        #                 stepping stones
+        #                 gaps, 
+        #                 smooth flat]
+        terrain_proportions = [0.0, 0.15, 0.2, 0.15, 0.0, 0.0, 0.4, 0.1]
+        # terrain_proportions = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
         # trimesh only:
-        slope_treshold = 1.5# slopes above this threshold will be corrected to vertical surfaces
-        origin_zero_z = True
-
-        num_goals = 8
+        slope_treshold = 0.75 # slopes above this threshold will be corrected to vertical surfaces
 
     class commands:
         curriculum = False
         max_curriculum = 1.
         num_commands = 4 # default: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
-        resampling_time = 6. # time before command are changed[s]
+        resampling_time = 10. # time before command are changed[s]
         heading_command = True # if true: compute ang vel command from heading error
         
-        lin_vel_clip = 0.2
-        ang_vel_clip = 0.4
         # Easy ranges
         class ranges:
-            lin_vel_x = [0., 1.5] # min max [m/s]
+            lin_vel_x = [0.35, 0.35] # min max [m/s]
             lin_vel_y = [0.0, 0.0]   # min max [m/s]
             ang_vel_yaw = [0, 0]    # min max [rad/s]
             heading = [0, 0]
 
-        # Easy ranges
-        class max_ranges:
-            lin_vel_x = [0.3, 0.8] # min max [m/s]
-            lin_vel_y = [-0.3, 0.3]#[0.15, 0.6]   # min max [m/s]
-            ang_vel_yaw = [-0, 0]    # min max [rad/s]
-            heading = [-1.6, 1.6]
+        # Full ranges
+        # class ranges:
+        #     lin_vel_x = [-1.0, 1.0] # min max [m/s]
+        #     lin_vel_y = [-1.0, 1.0]   # min max [m/s]
+        #     ang_vel_yaw = [-1, 1]    # min max [rad/s]
+        #     heading = [-3.14, 3.14]
 
-        class crclm_incremnt:
-            lin_vel_x = 0.1 # min max [m/s]
-            lin_vel_y = 0.1  # min max [m/s]
-            ang_vel_yaw = 0.1    # min max [rad/s]
-            heading = 0.5
-
-        waypoint_delta = 0.7
 
     class init_state:
         pos = [0.0, 0.0, 1.] # x,y,z [m]
@@ -271,55 +167,62 @@ class LeggedRobotCfg(BaseConfig):
 
     class domain_rand:
         randomize_friction = True
-        friction_range = [0.6, 2.]
-        randomize_base_mass = True
-        added_mass_range = [0., 3.]
-        randomize_base_com = True
-        added_com_range = [-0.2, 0.2]
+        friction_range = [0.5, 1.25]
+        randomize_base_mass = False
+        added_mass_range = [-1., 1.]
         push_robots = True
-        push_interval_s = 8
-        max_push_vel_xy = 0.5
+        push_interval_s = 15
+        max_push_vel_xy = 1.
 
-        randomize_motor = True
-        motor_strength_range = [0.8, 1.2]
-
-        delay_update_global_steps = 24 * 8000
-        action_delay = False
-        action_curr_step = [1, 1]
-        action_curr_step_scratch = [0, 1]
-        action_delay_view = 1
-        action_buf_len = 8
-        
     class rewards:
         class scales:
-            # tracking rewards
-            tracking_goal_vel = 1.5
-            tracking_yaw = 0.5
-            # regularization rewards
-            lin_vel_z = -1.0
+            termination = -0.1
+            tracking_lin_vel = 20.0
+            tracking_ang_vel = -4.0
+            lin_vel_z = -2.0
             ang_vel_xy = -0.05
-            orientation = -1.
+            orientation = -0.
+            torques = -0.0
+            dof_vel = -0.
             dof_acc = -2.5e-7
-            collision = -10.
-            action_rate = -0.1
-            delta_torques = -1.0e-7
-            torques = -0.00001
-            hip_pos = -0.5
-            dof_error = -0.04
-            feet_stumble = -1
-            feet_edge = -1
-            
-        only_positive_rewards = True # if true negative total rewards are clipped at zero (avoids early termination problems)
-        tracking_sigma = 0.2 # tracking reward = exp(-error^2/sigma)
+            base_height = -0. 
+            feet_air_time =  0.0
+            collision = -1.
+            feet_stumble = -0.0 
+            action_rate = -0.01
+            stand_still = -0.
+            act_penalty = -0.1
+            sideways_penalty = -0.04
+            work = -0.001
+
+        only_positive_rewards = not  True # if true negative total rewards are clipped at zero (avoids early termination problems)
+        tracking_sigma = 0.25 # tracking reward = exp(-error^2/sigma)
         soft_dof_pos_limit = 1. # percentage of urdf limits, values above this limit are penalized
-        soft_dof_vel_limit = 1
-        soft_torque_limit = 0.4
+        soft_dof_vel_limit = 1.
+        soft_torque_limit = 1.
         base_height_target = 1.
-        max_contact_force = 40. # forces above this value are penalized
+        max_contact_force = 100. # forces above this value are penalized
 
+    class normalization:
+        class obs_scales:
+            lin_vel = 2.0
+            ang_vel = 0.25
+            dof_pos = 1.0
+            dof_vel = 0.05
+            height_measurements = 5.0
+        clip_observations = 100.
+        clip_actions = 100.
 
-
-
+    class noise:
+        add_noise = True
+        noise_level = 0.3 # scales other values
+        class noise_scales:
+            dof_pos = 0.01
+            dof_vel = 1.5
+            lin_vel = 0.1
+            ang_vel = 0.2
+            gravity = 0.05
+            height_measurements = 0.1
 
     # viewer camera:
     class viewer:
@@ -350,21 +253,52 @@ class LeggedRobotCfgPPO(BaseConfig):
     seed = 1
     runner_class_name = 'OnPolicyRunner'
  
+    class distil:
+        num_episodes = 10000
+        num_epochs = 10000
+        num_teacher_obs = 235 - 12 - 24 - 3
+        logging_interval = 5
+        save_interval = 1000  
+        epoch_save_interval = 10
+        batch_size = 1024
+        num_steps = 100
+        num_training_iters = 10
+        lr = 1e-3
+        training_device = "cuda:0"
+        max_buffer_length = 1000000
+        num_warmup_steps = 100
     class policy:
         init_noise_std = 1.0
-        continue_from_last_std = True
-        scan_encoder_dims = [128, 64, 32]
         actor_hidden_dims = [512, 256, 128]
         critic_hidden_dims = [512, 256, 128]
-        priv_encoder_dims = [64, 20]
         activation = 'elu' # can be elu, relu, selu, crelu, lrelu, tanh, sigmoid
+        use_depth_backbone = False
+        num_input_vis_obs = 10 * 32 * 32
+        num_output_vis_obs = 10 * 128
         # only for 'ActorCriticRecurrent':
         rnn_type = 'lstm'
         rnn_hidden_size = 512
         rnn_num_layers = 1
 
-        tanh_encoder_output = False
+    class teacher_policy:
+        init_noise_std = 1.0
+        actor_hidden_dims = [512, 256, 128]
+        critic_hidden_dims = [512, 256, 128]
+        activation = 'elu' # can be elu, relu, selu, crelu, lrelu, tanh, sigmoid
+        use_depth_backbone = False
+        backbone_type = "deepgait_coordconv"
+        num_input_vis_obs = 10 * 32 * 32
+        num_output_vis_obs = 1280
     
+    class student_policy:
+        init_noise_std = 1.0
+        actor_hidden_dims = [512, 256, 128]
+        critic_hidden_dims = [512, 256, 128]
+        activation = 'elu' # can be elu, relu, selu, crelu, lrelu, tanh, sigmoid
+        use_depth_backbone = False
+        backbone_type = "deepgait_coordconv"
+        num_input_vis_obs = 10 * 32 * 32
+        num_output_vis_obs = 1280
     class algorithm:
         # training params
         value_loss_coef = 1.0
@@ -373,41 +307,39 @@ class LeggedRobotCfgPPO(BaseConfig):
         entropy_coef = 0.01
         num_learning_epochs = 5
         num_mini_batches = 4 # mini batch size = num_envs*nsteps / nminibatches
-        learning_rate = 2.e-4 #5.e-4
+        learning_rate = 1.e-3 #5.e-4
         schedule = 'adaptive' # could be adaptive, fixed
         gamma = 0.99
         lam = 0.95
         desired_kl = 0.01
         max_grad_norm = 1.
-        # dagger params
-        dagger_update_freq = 20
-        priv_reg_coef_schedual = [0, 0.1, 2000, 3000]
-        priv_reg_coef_schedual_resume = [0, 0.1, 0, 1]
-    
-    class depth_encoder:
-        if_depth = LeggedRobotCfg.depth.use_camera
-        depth_shape = LeggedRobotCfg.depth.resized
-        buffer_len = LeggedRobotCfg.depth.buffer_len
-        hidden_dims = 512
-        learning_rate = 1.e-3
-        num_steps_per_env = LeggedRobotCfg.depth.update_interval * 24
+        priv_dims = 0
+        num_vis_obs = 0
+        teacher_alpha = 1.0
 
-    class estimator:
-        train_with_estimated_states = True
-        learning_rate = 1.e-4
-        hidden_dims = [128, 64]
-        priv_states_dim = LeggedRobotCfg.env.n_priv
-        num_prop = LeggedRobotCfg.env.n_proprio
-        num_scan = LeggedRobotCfg.env.n_scan
+    class teacher_runner:
+        policy_class_name = 'ActorCritic'
+        algorithm_class_name = 'PPO'
+        num_steps_per_env = 24 # per iteration
+        max_iterations = 1500 # number of policy updates
 
+        # logging
+        save_interval = 500 # check for potential saves every this many iterations
+        experiment_name = 'rough_a1'
+        run_name = ''
+        # load and resume
+        resume = False
+        load_run = -1 # -1 = last run
+        checkpoint = -1 # -1 = last saved model
+        resume_path = None # updated from load_run and chkpt
     class runner:
         policy_class_name = 'ActorCritic'
         algorithm_class_name = 'PPO'
         num_steps_per_env = 24 # per iteration
-        max_iterations = 50000 # number of policy updates
+        max_iterations = 1500 # number of policy updates
 
         # logging
-        save_interval = 100 # check for potential saves every this many iterations
+        save_interval = 500 # check for potential saves every this many iterations
         experiment_name = 'rough_a1'
         run_name = ''
         # load and resume

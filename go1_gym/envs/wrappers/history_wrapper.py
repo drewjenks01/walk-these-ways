34befe1408942178f927292f502688160d9d8ff1
drewjenks01 <awj@mit.edu>
2023-10-20 15:20:30 -0400

    update

--- go1_gym/envs/wrappers/history_wrapper.py
+++ go1_gym/envs/wrappers/history_wrapper.py
@@ -4,42 +4,86 @@ import torch
 import gym
 
 class HistoryWrapper(gym.Wrapper):
-    def __init__(self, env):
+    def __init__(self, env, args):
         super().__init__(env)
         self.env = env
+        self.exp = args.exp
 
         self.obs_history_length = self.env.cfg.env.num_observation_history
+        self.history_frame_skip = self.env.cfg.env.history_frame_skip
 
         self.num_obs_history = self.obs_history_length * self.num_obs
-
-       # print('here', self.obs_history_length, self.num_obs)
+        self.obs_history_buf = torch.zeros(self.env.num_envs, self.obs_history_length * self.history_frame_skip, self.num_obs, dtype=torch.float,
+                                       device=self.env.device, requires_grad=False)
         self.obs_history = torch.zeros(self.env.num_envs, self.num_obs_history, dtype=torch.float,
                                        device=self.env.device, requires_grad=False)
         self.num_privileged_obs = self.num_privileged_obs
 
+        self.reward_container.load_env(self)
+        
     def step(self, action):
         # privileged information and observation history are stored in info
         obs, rew, done, info = self.env.step(action)
         privileged_obs = info["privileged_obs"]
 
-        self.obs_history = torch.cat((self.obs_history[:, self.env.num_obs:], obs), dim=-1)
+        self.obs_history_buf = torch.cat((self.obs_history_buf[:, 1:, :], obs.unsqueeze(1)), dim=1)
+        self.obs_history = self.obs_history_buf[:, self.history_frame_skip-1::self.history_frame_skip, :].reshape(self.env.num_envs, -1)
+        assert self.obs_history[:, -self.num_obs:].allclose(obs[:, :]), "obs_history does not end with obs"
+
+        rew_enrg = self.env.energy_rew
+        bsz = self.env.num_envs // 2
+            
+        if 'enrg' in self.exp:
+            if 'eipo' in self.exp:
+                rew_eipo_ext = rew[:bsz]
+                rew_ext = rew[bsz:]
+                rew_int = rew_enrg[:bsz]
+                rew_ext_int = rew_enrg[bsz:]
+                rew = {'eipo_ext': rew_eipo_ext, 'ext': rew_ext, 'int': rew_int, 'ext_int': rew_ext_int}
+            elif 'const' in self.exp:
+                if 'enrg_orig' in self.exp or 'enrg_trkv' in self.exp:
+                    rew_ext = rew_enrg
+                    rew_int = rew
+                else:
+                    rew_ext = rew
+                    rew_int = rew_enrg
+                rew = {'ext': rew_ext, 'int': rew_int}
+            else:
+                rew_ext = rew_enrg
+                rew = {'ext': rew_ext}
+        else:
+            if self.exp == 'eipo_task_axlr':
+                rew_tsk_all = rew['task'][:]
+                rew_aux_all = rew['axlr'][:]
+                rew_eipo_ext = rew_tsk_all[:bsz]
+                rew_int = rew_aux_all[:bsz]
+                rew_ext = rew_tsk_all[bsz:]
+                rew_ext_int = rew_aux_all[bsz:]
+                rew = {'eipo_ext': rew_eipo_ext, 'ext': rew_ext, 'int': rew_int, 'ext_int': rew_ext_int}
+            else:
+                rew = rew + rew_enrg
+                rew = {'ext': rew}
         return {'obs': obs, 'privileged_obs': privileged_obs, 'obs_history': self.obs_history}, rew, done, info
 
     def get_observations(self):
         obs = self.env.get_observations()
         privileged_obs = self.env.get_privileged_observations()
-        self.obs_history = torch.cat((self.obs_history[:, self.env.num_obs:], obs), dim=-1)
+        self.obs_history_buf = torch.cat((self.obs_history_buf[:, 1:, :], obs.unsqueeze(1)), dim=1)
+        self.obs_history = self.obs_history_buf[:, self.history_frame_skip-1::self.history_frame_skip, :].reshape(self.env.num_envs, -1)
         return {'obs': obs, 'privileged_obs': privileged_obs, 'obs_history': self.obs_history}
 
     def reset_idx(self, env_ids):  # it might be a problem that this isn't getting called!!
+        # print("RESETIDX")
         ret = super().reset_idx(env_ids)
+        self.obs_history_buf[env_ids, :, :] = 0
         self.obs_history[env_ids, :] = 0
         return ret
 
     def reset(self):
         ret = super().reset()
         privileged_obs = self.env.get_privileged_observations()
-        self.obs_history[:, :] = 0
+        with torch.inference_mode():
+            self.obs_history[:, :] = 0
         return {"obs": ret, "privileged_obs": privileged_obs, "obs_history": self.obs_history}
 
 

57dec2eb515f51b44e3f38349467c349a050cb90
drewjenks01 <awj@mit.edu>
2023-10-20 15:04:48 -0400

    update

--- go1_gym/utils/terrain.py
+++ go1_gym/utils/terrain.py
@@ -283,3 +283,49 @@ class Terrain:
 
         cfg.env_origins[i, j] = [env_origin_x, env_origin_y, env_origin_z]
 
+
+def perlin(x, y, octaves, lacunarity, gain, seed=0):
+    np.random.seed(seed)
+    total = 0
+    amplitude = 1.0
+    for _ in range(octaves):
+        total += perlin_layer(x, y) * amplitude
+        x *= lacunarity
+        y *= lacunarity
+        amplitude *= gain
+    return total
+    
+def perlin_layer(x, y):
+    # permutation table
+    p = np.arange(10000, dtype=int)
+    np.random.shuffle(p)
+    p = np.stack([p, p]).flatten()
+    # coordinates of the top-left
+    xi, yi = x.astype(int), y.astype(int)
+    # internal coordinates
+    xf, yf = x - xi, y - yi
+    # fade factors
+    u, v = fade(xf), fade(yf)
+    # noise components
+    n00 = gradient(p[p[xi] + yi], xf, yf)
+    n01 = gradient(p[p[xi] + yi + 1], xf, yf - 1)
+    n11 = gradient(p[p[xi + 1] + yi + 1], xf - 1, yf - 1)
+    n10 = gradient(p[p[xi + 1] + yi], xf - 1, yf)
+    # combine noises
+    x1 = lerp(n00, n10, u)
+    x2 = lerp(n01, n11, u)  # FIX1: I was using n10 instead of n01
+    return lerp(x1, x2, v)  # FIX2: I also had to reverse x1 and x2 here
+
+def lerp(a, b, x):
+    "linear interpolation"
+    return a + x * (b - a)
+
+def fade(t):
+    "6t^5 - 15t^4 + 10t^3"
+    return 6 * t**5 - 15 * t**4 + 10 * t**3
+
+def gradient(h, x, y):
+    "grad converts h to the right gradient vector and return the dot product with (x,y)"
+    vectors = np.array([[0, 1], [0, -1], [1, 0], [-1, 0]])
+    g = vectors[h % 4]
+    return g[:, :, 0] * x + g[:, :, 1] * y
